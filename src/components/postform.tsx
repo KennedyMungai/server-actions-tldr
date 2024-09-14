65133a0474c7eb1c888b15e1b71535abbca6b78c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-14 08:49:50 +0300

    Created a postform

--- src/components/postform.tsx
+++ src/components/postform.tsx
@@ -1,5 +1,58 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+const formSchema = z.object({
+	content: z.string().min(2, { message: "Post is required" }),
+});
+
 const PostForm = () => {
-	return <div>PostForm</div>;
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			content: "",
+		},
+	});
+
+	const onSubmit = async (values: z.infer<typeof formSchema>) =>
+		console.log(values);
+
+	return (
+		<main>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+					<FormField
+						control={form.control}
+						name="content"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Post</FormLabel>
+								<FormControl>
+									<Input placeholder="What's on your mind?" {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<Button type="submit" disabled={false}>
+						Submit
+					</Button>
+				</form>
+			</Form>
+		</main>
+	);
 };
 
 export default PostForm;
