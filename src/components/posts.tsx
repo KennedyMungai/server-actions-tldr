a73492d57f756a6c67a3d652e05520d77420b167
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-14 09:22:42 +0300

    Rendered all the posts

--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -1,5 +1,16 @@
-const Posts = () => {
-	return <div>Posts</div>;
+import { fetchPosts } from "@/server/actions/fetch-posts";
+import "server-only";
+
+const Posts = async () => {
+	const fetchedPosts = await fetchPosts().then((res) => res.success);
+
+	return (
+		<div>
+			{fetchedPosts.map((post) => (
+				<p key={post.id}>{post.content}</p>
+			))}
+		</div>
+	);
 };
 
 export default Posts;
