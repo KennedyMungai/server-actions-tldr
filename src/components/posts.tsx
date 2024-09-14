6297537ee064adc8a4d9b3df87ee8aa1aa661729
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-14 09:24:07 +0300

    Updated the way posts are rendered

--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -5,10 +5,12 @@ const Posts = async () => {
 	const fetchedPosts = await fetchPosts().then((res) => res.success);
 
 	return (
-		<div>
-			{fetchedPosts.map((post) => (
-				<p key={post.id}>{post.content}</p>
-			))}
+		<div className="max-w-4xl mx-auto mt-4">
+			<ul className="space-y-4">
+				{fetchedPosts.map((post) => (
+					<li key={post.id}>{post.content}</li>
+				))}
+			</ul>
 		</div>
 	);
 };
